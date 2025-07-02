32d39212d5bfb3e3a6484978036d80b7586b860c
tanyaofei <tan.yaofei@outlook.com>
2025-07-02 16:50:51 +0800

    添加 module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author tanyaofei
+ * @since 2025/7/2
+ **/
+module io.github.tanyaofei.copier {
+    requires jakarta.annotation;
+    requires cglib;
+    requires org.objectweb.asm;
+    requires org.apache.commons.lang3;
+    requires java.desktop;
+    exports io.github.tanyaofei.copier;
+}
